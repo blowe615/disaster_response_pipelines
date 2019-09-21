e9b6243296ef59b32d2282a87614ea3cf8329e51
Brandon Lowe <brandon.lowe615@gmail.com>
2019-09-21 12:05:31 -0500

    feat: Create process_data script to run ETL pipeline
    process_data requires the following inputs:
    - messages_filepath
    - categories_filepath
    - database_filepath
    - table_name (optional)
    
    The script loads the messages and categories data, cleans the
    datasets by merging them, parsing the categories into separate columns,
    and removing duplicates, then saves the data to the database provided.
    If table_name is provided, it will save it under that name, otherwise
    it is named 'merged'.

--- data/process_data.py
+++ data/process_data.py
@@ -1,22 +1,64 @@
+# import libraries
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
 
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
 
-def save_data(df, database_filename):
-    pass  
+    # use this row to extract a list of new column names for categories by dropping the last two characters
+    category_colnames = [x[:-2] for x in row]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # replace the original 'category' column in df with categories df
+    # drop the original categories column from `df`
+    df.drop(columns='categories',inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.merge(df,categories,left_index=True,right_index=True)
+
+    # drop duplicates from the 'message' column
+    df.drop_duplicates(subset='message',inplace=True)
+
+    return df
+
+def save_data(df, database_filename, table_name):
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
 
 
 def main():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        table_name = 'merged'
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
@@ -24,19 +66,36 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
+
+        print('Saving data...\n    DATABASE: {}\n    TABLE: {}'.format(database_filepath, table_name))
+        save_data(df, database_filepath, table_name)
+
         print('Cleaned data saved to database!')
-    
+
+    elif len(sys.argv) == 5:
+
+        messages_filepath, categories_filepath, database_filepath, table_name = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}\n    TABLE: {}'.format(database_filepath, table_name))
+        save_data(df, database_filepath, table_name)
+
+        print('Cleaned data saved to database!')
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
               'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
+              'to as the third argument. Optional: provide the table name to '\
+              'save to as well.\n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+              'DisasterResponse.db (optional)merged')
 
 
 if __name__ == '__main__':
